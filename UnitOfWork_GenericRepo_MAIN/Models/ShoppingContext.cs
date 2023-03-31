2dce0a425c89378473b1fdb196ce22a1b2386874
BhaveshKumarReddy <Admin@DESKTOP-HHR16QE>
2023-03-31 15:52:57 +0530

    Add project files.

---
+++ UnitOfWork_GenericRepo_MAIN/Models/ShoppingContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UnitOfWork_GenericRepo_MAIN.Models
+{
+    public partial class ShoppingContext : DbContext
+    {
+        public ShoppingContext()
+        {
+        }
+
+        public ShoppingContext(DbContextOptions<ShoppingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-HHR16QE\\SQLEXPRESS;Database=Shopping;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Eid);
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(10)
+                    .HasColumnName("phoneNumber");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.ToTable("Order");
+
+                entity.HasIndex(e => e.Pid, "IX_Order_PId");
+
+                entity.Property(e => e.OrderDate).HasColumnName("orderDate");
+
+                entity.Property(e => e.Pid).HasColumnName("PId");
+
+                entity.HasOne(d => d.PidNavigation)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.Pid);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.Pid);
+
+                entity.ToTable("Product");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
